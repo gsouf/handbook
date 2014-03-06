2ebd7f9c298642bb0a248de9e6ed9aaa3ce978ae
Soufiane Ghzal <sghzal@gmail.com>
2014-03-06 13:08:41 +0100

    Update webdev-tools.sh

--- install/webdev-tools.sh
+++ install/webdev-tools.sh
@@ -1,5 +1,8 @@
-
 echo "installing sprite-factory (https://github.com/jakesgordon/sprite-factory)"
 sudo apt-get install imageMagick libMagickWand-dev -y
 sudo gem install sprite-factory
 sudo gem install rmagick
+
+echo "installing png tools : pngq and pngcrush"
+sudo apt-get install pngcrush -y
+sudo apt-get install pngnq -y
