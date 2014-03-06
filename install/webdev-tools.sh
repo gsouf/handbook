3faaa6305173f38130ace1c9514243c5f65311be
Soufiane Ghzal <sghzal@gmail.com>
2014-03-06 12:42:16 +0100

    Create webdev-tools.sh

---
+++ install/webdev-tools.sh
@@ -0,0 +1,5 @@
+
+echo "installing sprite-factory (https://github.com/jakesgordon/sprite-factory)"
+sudo apt-get install imageMagick libMagickWand-dev -y
+sudo gem install sprite-factory
+sudo gem install rmagick
